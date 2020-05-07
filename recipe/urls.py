7fde7263949afab0bcecae4c65c25756055ab10e
Tiffany McLean <tamclean324@gmail.com>
2020-05-07 15:28:38 -0400

    adds a genericform for adding authors and recipes.  I still need to link them to the base.html file

--- recipe/urls.py
+++ recipe/urls.py
@@ -3,8 +3,11 @@ from recipe import views
 
 
 urlpatterns = [
-    path('', views.index),
+    path('', views.index, name='homepage'),
     path('recipe/<int:pk>/', views.recipe_detail, name='recipe_detail'),
-    path('author/<int:pk>/', views.author_detail, name='author')
+    path('recipeadd/', views.recipeadd),
+    path('authoradd/', views.authoradd),
+    path('author/<int:pk>/', views.author_detail, name='author'),
+
 
 ]
